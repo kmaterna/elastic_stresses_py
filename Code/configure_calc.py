308fde59aba7309380cebca3ba77ce910c797311
kmaterna <kathrynmaterna@gmail.com>
2019-03-01 15:39:58 -0800

    afterslip calculations done

--- Code/configure_calc.py
+++ Code/configure_calc.py
@@ -29,15 +29,15 @@ def configure_stress_calculation():
 	# title="Coulomb Stresses for 2016 M6.6";
 
 	# PARAMETER SET: THE 2014 AFTERSLIP
-	# exp_name = "M6.8_2014_afterslip"
-	# input_file = "../Inputs/"+exp_name+".intxt";
-	# aftershocks="../Inputs/20140310_aftershocks_ncsn.txt";
-	# title="Afterslip for 2014 M6.8";
+	exp_name = "M6.8_2014_afterslip"
+	input_file = "../Inputs/"+exp_name+".intxt";
+	aftershocks="../Inputs/20140310_aftershocks_ncsn.txt";
+	title="Afterslip for 2014 M6.8";
 
 	# PARAMETER SET: THE 2016 AFTERSLIP
-	exp_name = "M6.6_2016_afterslip"
-	input_file = "../Inputs/"+exp_name+".intxt";
-	title="Afterslip for 2016 M6.6";
+	# exp_name = "M6.6_2016_afterslip"
+	# input_file = "../Inputs/"+exp_name+".intxt";
+	# title="Afterslip for 2016 M6.6";
 
 	outdir="../Outputs/"+exp_name+"/";
 
