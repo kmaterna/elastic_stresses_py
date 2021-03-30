0947915de539162a4e063466f7cca491fc099245
kmaterna <kathrynmaterna@gmail.com>
2021-03-30 16:33:44 -0700

    read/write functions for static1D file formats

--- PyCoulomb/fault_slip_object/io_static1d.py
+++ PyCoulomb/fault_slip_object/io_static1d.py
@@ -1,13 +1,58 @@
 """
 Functions to read and write STATIC1D input files for fault slip and displacement at GPS points
 """
+import numpy as np
+from Tectonic_Utils.geodesy import fault_vector_functions
+from . import fault_slip_object
 
 
-def write_static1D_file(_fault_dict_list, _disp_points, _filename):
+def write_static1D_file(fault_dict_list, disp_points, filename):
+    """
+    Write a slip source and a list of GPS points into an input file for static1D
+    """
+    print("Writing static1d file %s " % filename);
+    ofile = open(filename, 'w');
+
+    # Choosing first fault.  Possibly only able to do one at a time.
+    one_fault = fault_dict_list[0];
+    top, bottom = fault_vector_functions.get_top_bottom_from_top(one_fault["depth"], one_fault["width"],
+                                                                 one_fault["dip"]);
+    ofile.write("{0:<5.1f}".format(np.round(bottom, 1)));
+    ofile.write("{0:<6.1f}".format(np.round(top, 1)));
+    ofile.write("{0:<6.1f}\n".format(np.round(one_fault["dip"], 1)));
+    ofile.write("1\n");
+    lons, lats = fault_slip_object.get_four_corners_lon_lat(one_fault);
+    fault_lon = lons[2];
+    fault_lat = lats[2];  # the deeper edge towards the strike direction
+    ofile.write(" %f %f %.2f %.2f %.2f %.2f \n" % (fault_lon, fault_lat, one_fault["length"], one_fault["strike"],
+                                                   one_fault["rake"], one_fault["slip"]*100) );  # lon/lat etc
+
+    ofile.write("%d\n" % len(disp_points.lon));
+    for i in range(len(disp_points.lon)):
+        ofile.write("{0:>13f}".format(disp_points.lon[i]) );
+        ofile.write("{0:>13f}\n".format(disp_points.lat[i]));
+    ofile.close();
     return;
 
 
-def read_static1D_file(_filename):
+def read_static1D_input_file(_filename):
+    """Not written yet"""
     fault_dict_list = [];
     disp_points = [];
     return fault_dict_list, disp_points;
+
+
+def read_static1D_output_file(output_filename, _input_filename):
+    """
+    Read the displacements from the output of a Static-1D calculation.
+    Input_filename will eventually be implemented to make the displacements and lon/lat into one object.
+    """
+    print("Reading file %s " % output_filename);
+    ifile = open(output_filename);
+    xdisp, ydisp, zdisp = [], [], [];
+    for line in ifile:
+        xdisp.append(float(line[20:33]));
+        ydisp.append(float(line[34:46]));
+        zdisp.append(float(line[47:59]));
+    ifile.close();
+    return xdisp, ydisp, zdisp;
--- PyCoulomb/fault_slip_object/plot_fault_slip.py
+++ PyCoulomb/fault_slip_object/plot_fault_slip.py
@@ -5,7 +5,8 @@ import pygmt
 from Elastic_stresses_py.PyCoulomb.fault_slip_object import fault_slip_object
 
 
-def map_source_slip_distribution(fault_dict_list, outfile, disp_points=None, region=None):
+def map_source_slip_distribution(fault_dict_list, outfile, disp_points=None,
+                                 disp_x=None, disp_y=None, _disp_z=None, region=None):
     """
     Plot a map of slip distribution from fault_dict_list, a general format for slip distributions.
     In order to use this function with other formats, like intxt or slippy, convert to the internal fault dict first.
@@ -31,6 +32,13 @@ def map_source_slip_distribution(fault_dict_list, outfile, disp_points=None, reg
     fig.coast(region=region, projection=proj, N='2', W='0.5p,black', L="g-125.5/39.6+c1.5+w50");
     if disp_points:
         fig.plot(x=disp_points.lon, y=disp_points.lat, style='s0.07i', color='blue', pen="thin,black");
+    if disp_x:
+        scale = (np.max(np.abs(disp_y)) * 0.002 / 0.012);  # empirical scaling to get convenient display
+        fig.plot(x=disp_points.lon, y=disp_points.lat, style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
+                 direction=[disp_x, disp_y], pen="thin,black");
+        fig.plot(x=[region[0]+0.30], y=[region[2]+0.05],  style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
+                 direction=[[10], [0]],  pen="thin,black");  # scale vector
+        fig.text(x=[region[0]+0.45], y=[region[2]+0.15], text='10 cm model');  # scale label
     fig.colorbar(D="jBr+w3.5i/0.2i+o2.5c/1.5c+h", C="mycpt.cpt", I="0.8",
                  G=str(vmin) + "/" + str(vmax), B=["x" + str(0.05), "y+L\"Slip(m)\""]);
     fig.savefig(outfile);
