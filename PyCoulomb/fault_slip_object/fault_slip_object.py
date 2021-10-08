eee37f61f0cc02f3ac3b5fa35e3bee49f36ece1d
kmaterna <kathrynmaterna@gmail.com>
2021-10-07 21:13:03 -0700

    first draft of vertical slip distribution gmt plot

--- PyCoulomb/fault_slip_object/fault_slip_object.py
+++ PyCoulomb/fault_slip_object/fault_slip_object.py
@@ -140,3 +140,42 @@ def write_gmt_fault_file(fault_dict_list, outfile):
         ofile.write("%f %f\n" % (lons[0], lats[0]));
     ofile.close();
     return;
+
+def write_gmt_vertical_fault_file(fault_dict_list, outfile):
+    """
+    Write the vertical coordinates of planar fault patches (length and depth, in local coords instead of lon/lat)
+    and associated slip values into a multi-segment file for plotting in GMT.
+    Good for vertical faults.
+    """
+    print("Writing file %s " % outfile);
+
+    # Get the origin: the extremal patch at the top
+    depth_array = [x["depth"] for x in fault_dict_list];
+    top_row_patches = [x for x in fault_dict_list if x['depth'] == np.nanmin(depth_array)];
+    top_row_x, top_row_y = [], [];
+    temp_origin = [top_row_patches[0]["lon"], top_row_patches[0]["lat"]];
+    for patch in top_row_patches:
+        [source] = fault_dict_to_coulomb_fault([patch], zerolon_system=temp_origin[0], zerolat_system=temp_origin[1]);
+        [_, _, x_updip, y_updip] = conversion_math.get_fault_four_corners(source);
+        top_row_x = top_row_x + x_updip;
+        top_row_y = top_row_y + y_updip;
+    toplons, toplats = fault_vector_functions.xy2lonlat(top_row_x, top_row_y, temp_origin[0], temp_origin[1]);
+
+    origin_ll = [np.min(toplons), np.min(toplats)];  # the extremal point.  THIS WILL BREAK IN GENERAL.
+
+    ofile = open(outfile, 'w');
+    for fault in fault_dict_list:
+        [source] = fault_dict_to_coulomb_fault([fault], zerolon_system=origin_ll[0], zerolat_system=origin_ll[1]);
+        [_, _, x_updip, y_updip] = conversion_math.get_fault_four_corners(source);
+        deeper_offset = fault["width"]*np.sin(np.deg2rad(fault["dip"]));
+        start_x = fault_vector_functions.get_vector_magnitude([x_updip[0], y_updip[0]]);
+        finish_x = fault_vector_functions.get_vector_magnitude([x_updip[1], y_updip[1]]);
+        ofile.write("> -Z"+str(fault['slip'])+"\n");
+        ofile.write("%f %f\n" % (start_x, fault["depth"]));
+        ofile.write("%f %f\n" % (finish_x, fault["depth"]));
+        ofile.write("%f %f\n" % (finish_x, fault["depth"]+deeper_offset));
+        ofile.write("%f %f\n" % (start_x, fault["depth"]+deeper_offset));
+        ofile.write("%f %f\n" % (start_x, fault["depth"]));
+
+    ofile.close();
+    return;
