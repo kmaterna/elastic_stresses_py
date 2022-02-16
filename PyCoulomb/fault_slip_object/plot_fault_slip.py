cdb8f9cb6a98150ea0a50c8c62867756bd0e3bab
kmaterna <kathrynmaterna@gmail.com>
2022-02-16 09:33:17 -0800

    pygmt vector plots no longer have to plot nans

--- PyCoulomb/fault_slip_object/plot_fault_slip.py
+++ PyCoulomb/fault_slip_object/plot_fault_slip.py
@@ -7,6 +7,10 @@ from .. import utilities
 
 
 def unpack_disp_points(disp_points):
+    """
+    Unpack all displacement points.
+    Also unpack any displacement points that have vertical data
+    """
     lon = np.array([x.lon for x in disp_points]);
     lat = np.array([x.lat for x in disp_points]);
     disp_x = np.array([x.dE_obs for x in disp_points]);
@@ -16,6 +20,14 @@ def unpack_disp_points(disp_points):
     lat_vert = np.array([x.lat for x in disp_points if ~np.isnan(x.dU_obs)]);
     disp_z_vert = np.array([x.dU_obs for x in disp_points if ~np.isnan(x.dU_obs)]);
     return [lon, lat, disp_x, disp_y, disp_z, lon_vert, lat_vert, disp_z_vert];
+
+def unpack_horiz_disp_points_for_vectors(disp_points):
+    """Unpack any displacement points that have horizontal data; will be plotted as vectors"""
+    lon_horiz = np.array([x.lon for x in disp_points if ~np.isnan(x.dE_obs)]);
+    lat_horiz = np.array([x.lat for x in disp_points if ~np.isnan(x.dE_obs)]);
+    disp_x_horiz = np.array([x.dE_obs for x in disp_points if ~np.isnan(x.dE_obs)]);
+    disp_y_horiz = np.array([x.dN_obs for x in disp_points if ~np.isnan(x.dE_obs)]);
+    return [lon_horiz, lat_horiz, disp_x_horiz, disp_y_horiz];
 
 
 def map_source_slip_distribution(fault_dict_list, outfile, disp_points=(), region=None,
@@ -59,20 +71,22 @@ def map_source_slip_distribution(fault_dict_list, outfile, disp_points=(), regio
             fig.plot(x=item[0], y=item[1], pen="thickest,darkred");
 
     if len(disp_points) > 0:
-        [lon, lat, disp_x, disp_y, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(disp_points);
+        [lon, lat, _, _, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(disp_points);
+        [lon_horiz, lat_horiz, disp_x_horiz, disp_y_horiz] = unpack_horiz_disp_points_for_vectors(disp_points);
         fig.plot(x=lon, y=lat, style='s0.07i', color='blue', pen="thin,black");
         if sum(~np.isnan(disp_z)) > 0:  # display vertical data if it's provided
             [v_cmap_opts, v_cbar_opts] = utilities.define_colorbar_series(disp_z_vert, tol=0.0001,
                                                                           v_labeling_interval=v_labeling_interval);
-            pygmt.makecpt(cmap="roma", series=str(v_cmap_opts[0])+"/" + str(v_cmap_opts[1]) + "/" +
-                                              str(v_cmap_opts[2]), background="o", output="vert.cpt");
+            series_str = str(v_cmap_opts[0])+"/" + str(v_cmap_opts[1]) + "/" + str(v_cmap_opts[2])
+            pygmt.makecpt(cmap="roma", series=series_str, background="o", output="vert.cpt");
             fig.plot(lon_vert, lat_vert, style='c0.3c', color=disp_z_vert, cmap='vert.cpt', pen="thin,black");
-            fig.colorbar(position="JCR+w4.0i+v+o0.7i/0i", cmap="vert.cpt", truncate=str(v_cbar_opts[0]) + "/" +
-                                                                                    str(v_cbar_opts[1]),
+            truncate_str = str(v_cbar_opts[0]) + "/" + str(v_cbar_opts[1]),
+            fig.colorbar(position="JCR+w4.0i+v+o0.7i/0i", cmap="vert.cpt", truncate=truncate_str,
                          frame=["x"+str(v_cbar_opts[2]), "y+L\"Vert Disp(m)\""]);
+        if sum(~np.isnan(disp_x_horiz) > 0):
             scale = scale_arrow[0] * (1/scale_arrow[1]);  # empirical scaling for convenient display
-            fig.plot(x=lon, y=lat, style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
-                     direction=[disp_x, disp_y], pen="thin,black");
+            fig.plot(x=lon_horiz, y=lat_horiz, style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
+                     direction=[disp_x_horiz, disp_y_horiz], pen="thin,black");
             fig.plot(x=[region[0]+0.30], y=[region[2]+1.05],  style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
                      direction=[[scale_arrow[1]], [0]],  pen="thin,black");  # scale vector
             fig.text(x=[region[0]+0.45], y=[region[2]+1.15], text=scale_arrow[2],
@@ -97,20 +111,20 @@ def plot_data_model_residual(outfile, disp_points, model_disp_points, resid_disp
         with fig.set_panel(panel=0):
             fig.basemap(region=region, projection=proj, frame=["WeSn", "2.0"]);
             fig.coast(region=region, projection=proj, borders='2', shorelines='1.0p,black', water='lightblue');
-            [lon, lat, disp_x, disp_y, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(disp_points);
+            [_, _, _, _, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(disp_points);
+            [lon_horiz, lat_horiz, dispx_horiz, dispy_horiz] = unpack_horiz_disp_points_for_vectors(disp_points);
 
             if sum(~np.isnan(disp_z)) > 0:  # display vertical data if it's provided
                 [cmap_opts, cbar_opts] = utilities.define_colorbar_series(disp_z_vert,
                                                                           v_labeling_interval=v_labeling_interval);
-                pygmt.makecpt(cmap="roma", series=str(cmap_opts[0]) + "/" +
-                                                  str(cmap_opts[1]) + "/" +
-                                                  str(cmap_opts[2]), background="o", output="vert.cpt");
+                series_str = str(cmap_opts[0]) + "/" + str(cmap_opts[1]) + "/" + str(cmap_opts[2]);
+                pygmt.makecpt(cmap="roma", series=series_str, background="o", output="vert.cpt");
                 fig.plot(lon_vert, lat_vert, projection=proj, style='c'+str(point_size)+'c', color=disp_z_vert,
                          cmap='vert.cpt', pen="0.1p,black");
 
             scale = scale_arrow[0] * (1/scale_arrow[1]);  # empirical scaling for convenient display
-            fig.plot(x=lon, y=lat, projection=proj, style='v0.12c+e+gblack+h0+p0.1p,black+z'+str(scale),
-                     direction=[disp_x, disp_y], pen="0.1p,black");
+            fig.plot(x=lon_horiz, y=lat_horiz, projection=proj, style='v0.12c+e+gblack+h0+p0.1p,black+z'+str(scale),
+                     direction=[dispx_horiz, dispy_horiz], pen="0.1p,black");
             fig.plot(x=[region[0]+0.30], y=[region[2]+1.05], projection=proj,
                      style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
                      direction=[[scale_arrow[1]], [0]],  pen="thin,black");  # scale vector
@@ -122,7 +136,8 @@ def plot_data_model_residual(outfile, disp_points, model_disp_points, resid_disp
         with fig.set_panel(panel=1):
             fig.basemap(region=region, projection=proj, frame=["WeSn", "2.0"]);
             fig.coast(region=region, projection=proj, borders='2', shorelines='1.0p,black', water='lightblue');
-            [lon, lat, disp_x, disp_y, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(model_disp_points);
+            [_, _, _, _, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(model_disp_points);
+            [lon_horiz, lat_horiz, dispx_horiz, dispy_horiz] = unpack_horiz_disp_points_for_vectors(model_disp_points);
 
             for item in fault_dict_list:
                 lons, lats = fault_slip_object.get_four_corners_lon_lat(item);
@@ -134,8 +149,8 @@ def plot_data_model_residual(outfile, disp_points, model_disp_points, resid_disp
                          cmap='vert.cpt', pen="0.1p,black");
 
             scale = scale_arrow[0] * (1/scale_arrow[1]);  # empirical scaling for convenient display
-            fig.plot(x=lon, y=lat, projection=proj, style='v0.12c+e+gblack+h0+p0.1p,black+z'+str(scale),
-                     direction=[disp_x, disp_y], pen="0.1p,black");
+            fig.plot(x=lon_horiz, y=lat_horiz, projection=proj, style='v0.12c+e+gblack+h0+p0.1p,black+z'+str(scale),
+                     direction=[dispx_horiz, dispy_horiz], pen="0.1p,black");
             fig.plot(x=[region[0]+0.30], y=[region[2]+1.05], projection=proj,
                      style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
                      direction=[[scale_arrow[1]], [0]],  pen="thin,black");  # scale vector
@@ -143,11 +158,12 @@ def plot_data_model_residual(outfile, disp_points, model_disp_points, resid_disp
             fig.text(position="TL", projection=proj, font="18p,Helvetica,black", fill="white", offset='j0.1/0.1',
                      text='B) Model');
 
-            # Residual
+        # Residual
         with fig.set_panel(panel=2):
             fig.basemap(region=region, projection=proj, frame=["WeSn", "2.0"]);
             fig.coast(region=region, projection=proj, borders='2', shorelines='1.0p,black', water='lightblue');
-            [lon, lat, disp_x, disp_y, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(resid_disp_points);
+            [_, _, _, _, disp_z, lon_vert, lat_vert, disp_z_vert] = unpack_disp_points(resid_disp_points);
+            [lon_horiz, lat_horiz, dispx_horiz, dispy_horiz] = unpack_horiz_disp_points_for_vectors(resid_disp_points);
 
             if sum(~np.isnan(disp_z)) > 0:  # display vertical data if it's provided
                 fig.plot(lon_vert, lat_vert, projection=proj, style='c'+str(point_size)+'c', color=disp_z_vert,
@@ -157,8 +173,8 @@ def plot_data_model_residual(outfile, disp_points, model_disp_points, resid_disp
                              frame=["x" + str(cbar_opts[2]), "y+L\"Vert Disp(m)\""]);
 
             scale = scale_arrow[0] * (1/scale_arrow[1]);  # empirical scaling for convenient display
-            fig.plot(x=lon, y=lat, projection=proj, style='v0.12c+e+gblack+h0+p0.1p,black+z'+str(scale),
-                     direction=[disp_x, disp_y], pen="0.1p,black");
+            fig.plot(x=lon_horiz, y=lat_horiz, projection=proj, style='v0.12c+e+gblack+h0+p0.1p,black+z'+str(scale),
+                     direction=[dispx_horiz, dispy_horiz], pen="0.1p,black");
             fig.plot(x=[region[0]+0.30], y=[region[2]+1.05], projection=proj,
                      style='v0.2c+e+gblack+h0+p1p,black+z'+str(scale),
                      direction=[[scale_arrow[1]], [0]],  pen="thin,black");  # scale vector
