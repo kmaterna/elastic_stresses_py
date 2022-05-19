dc0daaf2202e7af0994855c041819d563fecaa41
kmaterna <kathrynmaterna@gmail.com>
2022-05-18 17:34:24 -0700

    modifying outputs to gmt

--- PyCoulomb/disp_points_object/outputs.py
+++ PyCoulomb/disp_points_object/outputs.py
@@ -4,13 +4,16 @@ More specific than the basic ones listed in io_additionals.py
 """
 
 
-def write_disp_points_gmt(disp_points, filename):
+def write_disp_points_gmt(disp_points, filename, write_meas_type=False):
     """Write disp_points in GMT psvelo format"""
     print("Writing %s " % filename);
     ofile = open(filename, 'w');
     ofile.write("# lon lat dE dN dU Se Sn Su name\n");
     for item in disp_points:
         ofile.write("%f %f %f %f %f " % (item.lon, item.lat, item.dE_obs, item.dN_obs, item.dU_obs) );
-        ofile.write("%f %f %f %s\n" % (item.Se_obs, item.Sn_obs, item.Su_obs, item.name));
+        ofile.write("%f %f %f %s " % (item.Se_obs, item.Sn_obs, item.Su_obs, item.name));
+        if write_meas_type:
+            ofile.write("%s " % item.meas_type);
+        ofile.write("\n");
     ofile.close();
     return;
