ced89ef7eff19986152c80ad1d4801b7f9227269
kmaterna <kathrynmaterna@gmail.com>
2022-08-15 07:18:37 -0700

    small changes to api example

--- examples/API_example/demo_sourcedepth_API.py
+++ examples/API_example/demo_sourcedepth_API.py
@@ -5,19 +5,21 @@ from Elastic_stresses_py import PyCoulomb
 # My configuration: special experiment, focal mechanism as a function of depth.  This is my "lab notebook".
 # First column: depth.  Second column: Directory name.
 config = {
-    "exp1": [1.0, "depth1.0"],
-    "exp2": [2.0, "depth2.0"],
-    "exp3": [3.0, "depth3.0"],
-    "exp4": [4.0, "depth4.0"],
-    "exp5": [5.0, "depth5.0"],
-    "exp6": [6.0, "depth6.0"],
-    "exp7": [7.0, "depth7.0"]
+    "exp1": [1.0, "depth1.0/"],
+    "exp2": [2.0, "depth2.0/"],
+    "exp2.5": [2.5, "depth2.5/"],
+    "exp3": [3.0, "depth3.0/"],
+    "exp4": [4.0, "depth4.0/"],
+    "exp5": [5.0, "depth5.0/"],
+    "exp6": [6.0, "depth6.0/"],
+    "exp7": [7.0, "depth7.0/"]
 }
 
 def swap_output_dir(default_params, dirname):
     # write lines to replace params.outdir with the proper params.outdir
     # PACK MY SUITCASE
-    MyParams = PyCoulomb.configure_calc.modify_params_object(default_params, outdir=dirname);
+    old_outdir = '/'.join(default_params.outdir.split('/')[0:-2]);
+    MyParams = PyCoulomb.configure_calc.modify_params_object(default_params, outdir=old_outdir+"/"+dirname);
     return MyParams;
 
 
